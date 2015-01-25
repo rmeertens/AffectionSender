b180f2ce246db36fd92c832e637c688e7973a799
Roland Meertens <rolandmeertens@gmail.com>
2015-01-25 13:00:18 +0100

    Contact button working

--- app/src/main/java/com/meertens/affection_sender/HelloWidgetConfig.java
+++ app/src/main/java/com/meertens/affection_sender/HelloWidgetConfig.java
@@ -4,12 +4,16 @@ import java.io.FileNotFoundException;
 import android.app.Activity;
 import android.appwidget.AppWidgetManager;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Contacts;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
 
 public class HelloWidgetConfig extends Activity {
 
@@ -17,7 +21,8 @@ public class HelloWidgetConfig extends Activity {
 	
 	private int mAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
 	private static final String FILENAME = "klef.txt";
-	
+    private static final int CONTACT_PICKER_RESULT = 1337;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// Initialization code
@@ -98,14 +103,54 @@ public class HelloWidgetConfig extends Activity {
 
     // The on click listener for the contact button.
     private Button.OnClickListener configContactOnClickListener = new Button.OnClickListener() {
-        private static final int CONTACT_PICKER_RESULT = 1001;
+
 
         @Override
         public void onClick(View arg0) {
-            Intent contactPickerIntent = new Intent(Intent.ACTION_PICK,
-                    Contacts.CONTENT_URI);
-            startActivityForResult(contactPickerIntent, CONTACT_PICKER_RESULT);
+            Intent pickContactIntent = new Intent( Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI );
+            pickContactIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
+
+            startActivityForResult(pickContactIntent, CONTACT_PICKER_RESULT);
+
+
 
         }
     };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.i("OnResult","Got result");
+        // Check which request we're responding to
+        if (requestCode == CONTACT_PICKER_RESULT) {
+            Log.i("OnResult","Got result 2");
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                Log.i("OnResult","Got result 3");
+                // The user picked a contact.
+                // The Intent's data Uri identifies which contact was selected.
+
+                // Do something with the contact here (bigger example below)
+                Uri contactUri = data.getData();
+                // We only need the NUMBER column, because there will be only one row in the result
+                String[] projection = {ContactsContract.CommonDataKinds.Phone.NUMBER};
+
+                // Perform the query on the contact to get the NUMBER column
+                // We don't need a selection or sort order (there's only one result for the given URI)
+                // CAUTION: The query() method should be called from a separate thread to avoid blocking
+                // your app's UI thread. (For simplicity of the sample, this code doesn't do that.)
+                // Consider using CursorLoader to perform the query.
+                Cursor cursor = getContentResolver()
+                        .query(contactUri, projection, null, null, null);
+                cursor.moveToFirst();
+
+                // Retrieve the phone number from the NUMBER column
+                int column = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                String number = cursor.getString(column);
+                Log.i("Selected cntact", number);
+                AffectionInOutOperations.writeToFile("number.txt", number);
+                ((EditText)findViewById(R.id.phone)).setText(number);
+
+            }
+        }
+    }
 }
